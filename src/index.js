86a6550d5799dda0d2621926921cb6562bc2c96c
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-02-06 15:04:00 +0500

    Adding Service Wroker

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as serviceWorker from './serviceWorker';
 
 import { BrowserRouter, Route, Switch , NavLink } from 'react-router-dom'
 
 
-import Home from './component/home'
-import Posts from './component/posts'
-import Profile from './component/profile'
-import PostItem from './component/post_item'
-import Life from './component/lifecycles'
-import Conditional from './component/conditional'
-import User from './component/user'
+// import Home from './component/home'
+// import Posts from './component/posts'
+// import Profile from './component/profile'
+// import PostItem from './component/post_item'
+// import Life from './component/lifecycles'
+// import Conditional from './component/conditional'
+// import User from './component/user'
 
 const App = () =>{
 
@@ -20,27 +21,27 @@ const App = () =>{
 
             <header>
                     This is Header <br />
-                    <NavLink to="/">Home</NavLink><br/>
+                    {/* <NavLink to="/">Home</NavLink><br/>
                     <NavLink to="/posts" activeStyle={{color:'red'}} >Posts</NavLink><br />
                     <NavLink to={{
                         pathname:'/profile'
                     }}>Profile</NavLink><br />
                     <NavLink to="/life">Life</NavLink><br />
                     <NavLink to="/conditional">Conditional</NavLink><br />
-                    <NavLink to="/user">User</NavLink><br />
+                    <NavLink to="/user">User</NavLink><br /> */}
                     <hr/>
             </header>
             <Switch>
 
                   
-                    <Route path="/posts/:id/:username" component={PostItem} />
+                    {/* <Route path="/posts/:id/:username" component={PostItem} />
                     <Route path="/posts" component={Posts} />
                     <Route path="/profile" component={Profile} />
                     <Route path="/life" component={Life} />
                     <Route path="/conditional" component={Conditional} />
                     <Route path="/user" component={User} />
                     <Route path="/" exact component={Home} />
-                    <Route render={() => <h3>oops 404</h3>} />
+                    <Route render={() => <h3>oops 404</h3>} /> */}
             </Switch>
             </div>
         </BrowserRouter>
@@ -54,4 +55,4 @@ const App = () =>{
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-
+serviceWorker.register();
