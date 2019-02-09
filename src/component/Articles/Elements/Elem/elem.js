b3d3f261056639941320ba8f40e4ef63a82cf339
mohsinansari3011 <mohsinansari3011@gmail.com>
2019-02-09 20:37:29 +0500

    Create Elem.js

---
+++ src/component/Articles/Elements/Elem/elem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class elem extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default elem;
